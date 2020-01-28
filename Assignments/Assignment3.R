fd4c7f9cdb45e9ba20fd78a8ce94b126eae3ba73
Nathan Van Bibber <nvanbibber@mail.usf.edu>
2020-01-28 16:01:46 -0500

    corrected syntax errors from strange double quote symbol from pasting from canvas into .R file. Corrected and shortened variable names into correct syntax. Added missing comma in ABC_poll vector.

--- Assignments/Assignment3.R
+++ Assignments/Assignment3.R
@@ -1,5 +1,5 @@
-Name <- c("Jeb", “Donald”, "Ted”, “Marco” “Carly”, “Hillary”, “Berine”)
+Name <- c("Jeb", "Donald", "Ted", "Marco", "Carly", "Hillary", "Berine")
 
-ABC political poll results <- c(4, 62 51, 21, 2, 14, 15)
+ABC_poll <- c(4, 62, 51, 21, 2, 14, 15)
 
-CBS political poll results <- c(12, 75, 43, 19, 1, 21, 19) 
+CBS_poll <- c(12, 75, 43, 19, 1, 21, 19) 
