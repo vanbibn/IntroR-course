4eac3c0894733bf735111609d65826dcfeadbd37
Nathan Van Bibber <nvanbibber@mail.usf.edu>
2020-01-28 15:49:28 -0500

    add assingnment 3 as presented in canvas

---
+++ Assignments/Assignment3.R
@@ -0,0 +1,5 @@
+Name <- c("Jeb", “Donald”, "Ted”, “Marco” “Carly”, “Hillary”, “Berine”)
+
+ABC political poll results <- c(4, 62 51, 21, 2, 14, 15)
+
+CBS political poll results <- c(12, 75, 43, 19, 1, 21, 19) 
