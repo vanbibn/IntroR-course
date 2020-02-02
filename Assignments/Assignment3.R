f53361c66736386147fb03054541f4462a1e07bd
Nathan Van Bibber <nvanbibber@mail.usf.edu>
2020-02-01 22:41:43 -0500

    combined poll results into a data frame with candidate names as row names

--- Assignments/Assignment3.R
+++ Assignments/Assignment3.R
@@ -3,3 +3,5 @@ Name <- c("Jeb", "Donald", "Ted", "Marco", "Carly", "Hillary", "Berine")
 ABC_poll <- c(4, 62, 51, 21, 2, 14, 15)
 
 CBS_poll <- c(12, 75, 43, 19, 1, 21, 19) 
+
+poll_results <- data.frame(ABC_poll,CBS_poll, row.names = Name)
