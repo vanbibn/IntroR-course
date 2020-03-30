eb2a9d27bd2287ce245d96416e3c686d8189e9fe
Nathan Van Bibber <nvanbibber@mail.usf.edu>
2020-03-29 23:44:36 -0400

    Fixed bug with funModeling package and tukey_outlier() function.

--- Assignments/Assignment11.R
+++ Assignments/Assignment11.R
@@ -1,7 +1,12 @@
+
+
+install.packages("funModeling")
+library(funModeling)
+
 tukey_multiple <- function(x) {
     outliers <- array(TRUE,dim=dim(x))
     for (j in 1:ncol(x)) {
-        outliers[,j] <- outliers[,j] && tukey.outlier(x[,j])
+        outliers[,j] <- outliers[,j] && tukey_outlier(x[,j])
     }
     
     outlier.vec <- vector(length=nrow(x))
@@ -18,4 +23,8 @@ tukey_multiple(matrix(1:10, nrow = 2, byrow = T))
 # line 9 - need a comma after "i" in `outlier.vec[i]` --> `outlier.vec[i,]`
 
 # Reformatted with correct indentation to see it more clearly, apparently I 
-# unknownly fixed the 
+# unknownly fixed the error
+# Return needs to be on last line by itself?
+
+# tukey.outlier() function does not exist
+# I found a tukey_outlier() function in the funModeling package.
